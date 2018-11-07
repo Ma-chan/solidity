3213530c4d2bcef3d2eb48fdbe159478a891ded0
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-11-07 15:47:13 +0900

    Update HomeMadeCoin.sol

--- HomeMadeCoin.sol
+++ HomeMadeCoin.sol
@@ -2,12 +2,13 @@ pragma solidity ^0.4.19;
 
 contract HomeMadeCoin{
     mapping (address=>uint256) public balance;
+    event transfer(address from ,address to,uint256 value);
     function HomeMadeCoin(uint256 total){
         balance[msg.sender] = total;
     }
-    function transfer(address to, uint256 value){
-        balance[msg.sender] =- value
+    function Transfer(address to, uint256 value){
+        balance[msg.sender] -= value;
         balance[to] += value;
-        Transfer(msg.sender, to, value);
+        transfer(msg.sender, to, value);
     }
 }
