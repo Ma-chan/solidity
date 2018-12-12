abd3b20de4e2816894b15f109fabaaba3dac960c
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-12-12 15:59:48 +0900

    Create AOJContract.sol

---
+++ AOJContract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.19
+
+contract AOJContract{
+    struct {
+        string name;
+        
+    }
+    
+    mapping (address => ) public ;
+    //答案を送る関数
+    function transfer(address receiver, ){
+    }
+}       
