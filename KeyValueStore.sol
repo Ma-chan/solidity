31fc36d796da751d6dacd8d4600d85ee6f63387b
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2019-02-24 17:49:14 +0900

    Create KeyValueStore.sol

---
+++ KeyValueStore.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.23;
+
+contract KeyValueStore{
+    uint256 keyIndex;
+    
+    struct values{
+        string value1;
+        string value2;
+    }
+    
+    mapping (uint256 => values) Obj;
+    function setValue(string _value1, string _value2) view returns (uint256) 
+    {
+    Obj[keyIndex].value1 = _value1;
+    Obj[keyIndex].value2 = _value2;
+    keyIndex++;
+    return keyIndex;
+    }
+    
+    function getValue1(uint _key) view returns pure (string){
+        return Obj[_key].value1;
+    }
+    function getValue2(uint _key) view returns pure (string){
+        return Obj[_key].value2;
+    }
+}
