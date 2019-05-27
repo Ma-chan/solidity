6eb12dcabc6b077bd9fd729cda4cce2186306116
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2019-05-27 21:18:18 +0900

    Update resister.sol

--- resister.sol
+++ resister.sol
@@ -8,13 +8,20 @@ contract Block{
         address User_address;
         uint Accept_Number;
     }
-    //ユーザの情報を検索する関数
+    //ユーザの情報を引き出す関数
     function research (uint User_ID,string User_Name) private{
-        
+        if(Use_name ){
+             
+        }else if(User_ID ){
+            
+        }
     }
     //ユーザの情報を追加する関数
     function add (string User_Name,uint User_ID,)private{
+        Accept_Number += msg.value;
+        
         
     }
+    
     
 }
