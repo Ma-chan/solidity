f5b3425a5ac8d1eae070775f20b0e05d0d108e0c
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2019-05-27 18:47:58 +0900

    Create resister.sol

---
+++ resister.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.19;
+
+contract Block{
+    //ユーザの構造体
+    struct User{
+        string User_Name;
+        uint User_ID;
+        address User_address;
+        uint Accept_Number;
+    }
+    //ユーザの情報を検索する関数
+    function research (uint User_ID,string User_Name) private{
+        
+    }
+    //ユーザの情報を追加する関数
+    function add (string User_Name,uint User_ID,)private{
+        
+    }
+    
+}
