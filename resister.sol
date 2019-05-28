865a2d8ec51020edfa397e087b82ce1fdc7d8002
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2019-05-28 15:19:35 +0900

    Update resister.sol

--- resister.sol
+++ resister.sol
@@ -3,24 +3,24 @@ pragma solidity ^0.4.19;
 contract Block{
     //ユーザの構造体
     struct User{
-        string User_Name;
         uint User_ID;
         uint Problem_ID;
-        uint Accept_Number;
+        uint Accept_Accumulator;
         uint Result_hash;
         uint Code_hash;
     }
     //ユーザの情報を引き出す関数
-    function research (uint User_ID,string User_Name) private{
-        if(uint ,uint U){
-             
-        }else if(uint User_ID ){
-          
-        }
+    function research (uint User_ID) private {
+        
     }
     //ユーザの情報を追加する関数
-    function add (string User_Name,uint User_ID,)private{
+    function add (uint User_ID) private {
         Accept_Number += msg.value;
+        if(){
+            
+        }else if{
+            
+        }
     }
     
     
